166c73b754af9409d078c5aa90c7593f9a95ffd1
Reynaldo <gilcu2@gmail.com>
2015-05-18 09:24:41 +0200

    improving graph as container

--- src/main/scala/twitter/Hashtag.scala
+++ src/main/scala/twitter/Hashtag.scala
@@ -20,7 +20,8 @@ class Graph extends core.Graph[HashtagNode,Twittering] {
   def addUser(u:User):Unit=addNode(u.id,u,u.screen_name)
   def addHashtag(h:Hashtag)=addNode(h.id,h,h.name)
   def addTwittering(src:Long,dst:Long,tweetCount:Long,retweedCount:Long): Unit ={
-    val nSrc=getNode(src).asInstanceOf[User]
+    assert(getNode(src).isInstanceOf[User])
+    addEdge(src,dst,new Twittering(src,dst,tweetCount,retweedCount),directed = true)
 
   }
 }
